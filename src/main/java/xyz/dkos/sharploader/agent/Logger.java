024298e2557de466e507509beddd5093615ca3b4
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-07 16:27:23 +0800

    添加Logger的测试方法

---
+++ src/main/java/xyz/dkos/sharploader/agent/Logger.java
@@ -0,0 +1,24 @@
+package xyz.dkos.sharploader.agent;
+
+public class Logger {
+    // Native method declarations
+    public static native void all(String message);
+    public static native void trace(String message);
+    public static native void debug(String message);
+    public static native void info(String message);
+    public static native void warn(String message);
+    public static native void error(String message);
+    public static native void fatal(String message);
+    public static native void off(String message);
+
+    public static void test(){
+        Logger.all("(Test) Hello, World! ");
+        Logger.trace("(Test) Hello, World! ");
+        Logger.debug("(Test) Hello, World! ");
+        Logger.info("(Test) Hello, World! ");
+        Logger.warn("(Test) Hello, World! ");
+        Logger.error("(Test) Hello, World! ");
+        Logger.fatal("(Test) Hello, World! ");
+        Logger.off("(Test) Hello, World! ");
+    }
+}
