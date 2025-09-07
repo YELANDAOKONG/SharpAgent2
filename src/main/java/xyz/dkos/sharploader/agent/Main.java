50a098e080a3ac042df64d5a3ea96e5d545af7e1
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2025-09-07 18:23:07 +0800

    更新等待阻塞

--- src/main/java/xyz/dkos/sharploader/agent/Main.java
+++ src/main/java/xyz/dkos/sharploader/agent/Main.java
@@ -1,5 +1,36 @@
 package xyz.dkos.sharploader.agent;
 
+import java.lang.instrument.Instrumentation;
+
 public class Main {
 
+    public static void main(String[] args) {
+        System.err.println("Agent cannot be run directly. Please use -javaagent option.");
+        System.exit(1);
+    }
+
+    public static void agentmain(String args, Instrumentation inst) {
+        System.err.println("Agent cannot be attached after startup.");
+        System.exit(1);
+    }
+
+    public static volatile boolean initialized = false;
+
+    public static void premain(String args, Instrumentation inst) {
+        System.out.println("[+] Hello, World!");
+        System.out.println("Loader Agent initializing...");
+
+        System.out.println("Waiting for Loader initialization...");
+        while (!initialized) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Agent initialization interrupted.");
+                return;
+            }
+        }
+
+        System.out.println("Loader initialization completed. ");
+    }
 }
